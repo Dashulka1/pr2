32f8e65594314f4915267240fbed1584668a0e70
Dashulka1 <144533783+Dashulka1@users.noreply.github.com>
2025-03-05 20:46:57 +0300

    Add files via upload

---
+++ PR2-main/build/CMakeFiles/PR2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sa/Загрузки/PR2-main/src/errors.c" "/home/sa/Загрузки/PR2-main/build/CMakeFiles/PR2.dir/src/errors.c.o"
+  "/home/sa/Загрузки/PR2-main/src/logger.c" "/home/sa/Загрузки/PR2-main/build/CMakeFiles/PR2.dir/src/logger.c.o"
+  "/home/sa/Загрузки/PR2-main/src/main.c" "/home/sa/Загрузки/PR2-main/build/CMakeFiles/PR2.dir/src/main.c.o"
+  "/home/sa/Загрузки/PR2-main/src/processes.c" "/home/sa/Загрузки/PR2-main/build/CMakeFiles/PR2.dir/src/processes.c.o"
+  "/home/sa/Загрузки/PR2-main/src/users.c" "/home/sa/Загрузки/PR2-main/build/CMakeFiles/PR2.dir/src/users.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
